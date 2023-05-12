a711c389cf128fb2f9c292b2326375c9df93ec73
Anish Khadka <akhadka2@sfsu.edu>
2023-05-11 22:36:03 -0700

    Update dgemv-openmp-nested.cpp

--- dgemv-openmp-nested.cpp
+++ dgemv-openmp-nested.cpp
@@ -18,14 +18,14 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    int nthreads = omp_get_num_procs(); // Set to number of available processors
    omp_set_num_threads(nthreads);
 
-   // Parallelize the loop over rows of A
+   // Parallelize the outer loop over rows of A
    #pragma omp parallel for
    for (int i = 0; i < n; i++) {
-      double sum = 0.0;
+      // Parallelize the inner loop over columns of A
+      #pragma omp parallel for
       for (int j = 0; j < n; j++) {
-         sum += A[i*n+j] * x[j];
+         y[i] += A[i*n+j] * x[j];
       }
-      y[i] += sum;
    }
 
 }

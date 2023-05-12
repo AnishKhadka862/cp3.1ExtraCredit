718baeda12ebe1e3dc27f3934b1a1b0edfd41ea0
Anish Khadka <akhadka2@sfsu.edu>
2023-05-11 22:36:57 -0700

    Update dgemv-openmp-inner.cpp

--- dgemv-openmp-inner.cpp
+++ dgemv-openmp-inner.cpp
@@ -1,11 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+const char* dgemv_desc = "OpenMP dgemv.";
+
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
+
 void my_dgemv(int n, double* A, double* x, double* y) {
 
    // Set the number of threads to use
    int nthreads = omp_get_num_procs(); // Set to number of available processors
    omp_set_num_threads(nthreads);
 
-   // Parallelize the inner loop over columns of A
+
    for (int i = 0; i < n; i++) {
+      // Parallelize the inner loop over columns of A
       #pragma omp parallel for
       for (int j = 0; j < n; j++) {
          y[i] += A[i*n+j] * x[j];
